c6e43ab7dd08f4b9697be3faf029f5508ebc1df1
WhichWho <1366505103@qq.com>
2022-09-10 22:24:58 +0800

    upgrade dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("org.jsoup:jsoup:1.15.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     testImplementation(kotlin("test"))
 }
 
